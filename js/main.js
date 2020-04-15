c295e1517300e5fb6f49e71e5b1b9022b5b076d8
Nicolo <nicolorizzi1996@gmail.com>
2020-04-15 17:01:31 +0200

    FUNZIONE CHIEDI 5 NUMERI

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@
  * Un alert espone 5 numeri casuali (univoci).
  *  Da li parte un timer di 30 secondi.
  * Dopo 30 secondi l’ utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
- * Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
- * Assicuratevi di saper fare correttamente l’ esercizio con output base (alert() , prompt() , console.log() )
+ * Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
  */
 
  // PSEUDOCODICE
@@ -18,6 +17,7 @@
 
   var numberGames = [];
   var numGen;
+  var numberUser = [];
   for (var i = 0; i <=4; i++){
     numGen = rndmNumber();
     if (numberGames.includes(numGen) ) {
@@ -27,6 +27,8 @@
     }
   }
   alert("I numeri generati casualmente sono: " + numberGames);
+  setTimeout(get5Number, 2000);
+  
   
   
 
@@ -36,7 +38,17 @@
 
 
   // FUNZIONI
+  // FUNZIONE GENERA NUMERO RANDOM
   function rndmNumber () {
       var numRandom = Math.floor(Math.random() * 20) + 1;
       return numRandom;
     } 
+
+    // FUNZIONE CHIEDI NUMERI
+    function get5Number() {
+      for (var i = 0; i <=4; i++) {
+        numUser = parseInt(prompt('Inserisci il numero'));
+        numberUser.push(numUser);        
+      }
+      console.log(numberUser);
+    }
